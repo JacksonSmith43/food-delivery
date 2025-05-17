a19777853d27ba85529236bb7260152e600ebd60
JacksonSmith43 <jessica444@gmx.at>
2025-05-17 15:16:12 +0200

    Added easier to understand search foods.

--- src/scripts/order-page.js
+++ src/scripts/order-page.js
@@ -1,34 +1,32 @@
-fetch('../data/dishes.json')
-    .then(response => response.json()) // Once the file has successfuly been loaded the data from the HTTP response will be converted into a JSON object. // AKA: Convert the response to JSON (this returns a Promise).  
-    .then(data => {  // Use the JSON data once it's available. 
+addEventListener("DOMContentLoaded", (event) => {
 
-        for (let i = 0; i < data.length; i++) {
-            console.log(data[i].name);
-        }
-    })
-    .catch(error => {   // Handle any errors that might occur during fetch or parsing. 
-        console.error('Error fetching the JSON file:', error);
+    fetch('../data/dishes.json')
+        .then(response => response.json()) // Once the file has successfuly been loaded the data from the HTTP response will be converted into a JSON object. // AKA: Convert the response to JSON (this returns a Promise).  
+        .then(data => {  // Use the JSON data once it's available. 
+
+            for (let i = 0; i < data.length; i++) {
+                console.log(data[i].name);
+            }
+        })
+        .catch(error => {   // Handle any errors that might occur during fetch or parsing. 
+            console.error('Error fetching the JSON file:', error);
+        });
+
+
+    const foodSearchInput = document.getElementById("food-search-input");
+    const foodSearchList = document.querySelectorAll("#food-search-list li");
+
+    foodSearchInput.addEventListener("input", () => {
+        const filter = foodSearchInput.value.toUpperCase();
+
+        foodSearchList.forEach((li) => {
+            const text = li.textContent.toUpperCase();
+            console.log("text:", text);
+            console.log("li:", li);
+
+            li.style.display = text.includes(filter) ? "" : "none";
+        });
     });
 
-
-function searchesFoods() {
-    let foodSearchInput = document.getElementById("food-search-input");
-    let foodSearchList = document.querySelector("#food-search-list");
-
-    let filter, li, a, i, txtValue;
-    li = foodSearchList.getElementsByTagName("li");
-    filter = foodSearchInput.value.toUpperCase();
-
-    for (i = 0; i < li.length; i++) {
-        a = li[i].getElementsByTagName("a")[0];
-        txtValue = a.textContent || a.innerText;
-
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            li[i].style.display = "";
-
-        } else {
-            li[i].style.display = "none";
-        }
-    }
-}
+})
 
