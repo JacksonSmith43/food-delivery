b6e5f83b69b796e94b828b6db16c9780410aa2fb
JacksonSmith43 <jessica444@gmx.at>
2025-05-19 17:53:52 +0200

    Able to search dishes from the JSON file.

--- src/scripts/order-page.js
+++ src/scripts/order-page.js
@@ -1,33 +1,43 @@
 addEventListener("DOMContentLoaded", (event) => {
     const foodSearchInput = document.getElementById("food-search-input");
-    const foodSearchList = document.querySelectorAll("#food-search-list li");
+    const foodSearchList = document.getElementById("food-search-list");
+    let foodData = [];
 
-
-    fetch('../data/dishes.json')
-        .then(response => response.json()) // Once the file has successfuly been loaded the data from the HTTP response will be converted into a JSON object. // AKA: Convert the response to JSON (this returns a Promise).  
-        .then(data => {  // Use the JSON data once it's available. 
-
-            for (let i = 0; i < data.length; i++) {
-                console.log(data[i].name);
-            }
-        })
-        .catch(error => {   // Handle any errors that might occur during fetch or parsing. 
-            console.error('Error fetching the JSON file:', error);
-        });
-
+    fetchFoodData();
 
     foodSearchInput.addEventListener("input", () => {
 
-        const filter = foodSearchInput.value.toUpperCase();
+        const inputTerm = foodSearchInput.value.trim().toUpperCase();
 
-        foodSearchList.forEach((li) => {
-            const text = li.textContent.toUpperCase();
-            console.log("text:", text);
-            console.log("li:", li);
+        if (inputTerm === "") {
+            foodSearchList.innerHTML = "";
+            return;
 
-            li.style.display = text.includes(filter) ? "" : "none";
-        });
+        } else {
+            displayFoodList(inputTerm);
+        }
+
     });
+
+    async function fetchFoodData() {
+        const response = await fetch('../data/dishes.json') // Fetches the JSON file.
+        foodData = await response.json(); // Once the file has successfuly been loaded the data from the HTTP response will be converted into a JSON object. // AKA: Convert the response to JSON.  
+    }
+
+    function displayFoodList(inputTerm) {
+        foodSearchList.innerHTML = ""; // Empties the list. Makes sure that only valid results are displayed. 
+
+        foodData.forEach((item) => { // Iterates through all of the dishes. 
+
+            const foodName = item.name.toUpperCase();
+
+            if (foodName.includes(inputTerm)) {
+                const listItem = document.createElement("li");
+                listItem.textContent = item.name;
+                foodSearchList.appendChild(listItem);
+            }
+        });
+    }
 
 })
 
