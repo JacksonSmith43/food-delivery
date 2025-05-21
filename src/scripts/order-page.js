523dbb5f970bdd05262cda12a00463ada6fab661
JacksonSmith43 <jessica444@gmx.at>
2025-05-21 12:57:14 +0200

    Able to see (console) the appropriate restaurants from the food search.

--- src/scripts/order-page.js
+++ src/scripts/order-page.js
@@ -2,6 +2,7 @@ addEventListener("DOMContentLoaded", (event) => {
     const foodSearchInput = document.getElementById("food-search-input");
     const foodSearchList = document.getElementById("food-search-list");
     let foodData = [];
+    let restaurantData = [];
 
     fetchFoodData();
 
@@ -24,6 +25,11 @@ addEventListener("DOMContentLoaded", (event) => {
         foodData = await response.json(); // Once the file has successfuly been loaded the data from the HTTP response will be converted into a JSON object. // AKA: Convert the response to JSON.  
     }
 
+    async function fetchesRestaurant() {
+        const response = await fetch('../data/restaurants.json');
+        restaurantData = await response.json();
+    }
+
     function displayFoodList(inputTerm) {
         foodSearchList.innerHTML = ""; // Empties the list. Makes sure that only valid results are displayed. 
 
@@ -33,11 +39,46 @@ addEventListener("DOMContentLoaded", (event) => {
 
             if (foodName.includes(inputTerm)) {
                 const listItem = document.createElement("li");
+                listItem.setAttribute("tabindex", "0");
                 listItem.textContent = item.name;
                 foodSearchList.appendChild(listItem);
+
+                selectFoodItem(listItem, item);
             }
         });
     }
 
+    function selectFoodItem(listItem, foodItem) {
+        listItem.addEventListener("click", async () => {
+            await fetchesRestaurant();
+            await comparingFoodAndRestaurants(foodItem, restaurantData);
+        });
+    }
+
+    async function comparingFoodAndRestaurants(food, restaurantData) {
+        const restaurantIds = Array.isArray(food.restaurantId) // If it is not an array, turn it into one. Necessary for the JSON data. 
+            ? food.restaurantId
+            : [food.restaurantId];
+
+        const isRestaurant = restaurantData.filter((restaurant) =>
+            restaurantIds.includes(restaurant.id)
+        );
+
+        if (isRestaurant.length > 0) {
+            const restaurantName = isRestaurant.map(r => r.name);
+
+            console.log(`Restaurant IDs: ${food.restaurantId} Names: ${restaurantName}`);
+            console.log(`Food ID: ${food.id} Name: ${food.name}`);
+            showRestaurants(restaurantName);
+        } else {
+            console.log("No fitting restaurants have been found.");
+        }
+    }
+
+
+    function showRestaurants(restaurantName) {
+        const restaurantList = document.getElementById("food-search-results");
+        restaurantList.innerHTML = ""; 
+    }
 })
 
